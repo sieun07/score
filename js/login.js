258c5e3058e84e52faca396c7560c6b02bbfc15f
sieun07 <lion101027@gmail.com>
2025-05-05 02:45:34 +0900

    학생, 선생님 별 로그인 회원가입 완료. 그림 추가해야함

--- js/login.js
+++ js/login.js
@@ -1,17 +1,3 @@
-function NoMultiChk(chk) {
-    var obj = document.getElementsByName("who-check");
-    for (var i = 0; i < obj.length; i++) {
-        if (obj[i] != chk) {
-            obj[i].checked = false;
-        }
-    }
-}
 
-function OnSave() {
-    var cob_check = document.querySelectorAll('input[name="who-check"]:checked').length;
-    if (cob_check == 0) {
-        alert('체크박스를 하나 이상 선택해주세요')
-        return false;
-    }
-}
+
 
