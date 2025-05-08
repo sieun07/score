9e141584ce691ed473837fc2dd31ab360af11106
sieun07 <lion101027@gmail.com>
2025-05-09 00:52:45 +0900

    ..최종 (메인페이지 간격 수정)

--- js/mainpage_stu.js
+++ js/mainpage_stu.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const rows = document.querySelectorAll(".log tbody tr");
     rows.forEach((row, index) => {
-        if (index >= 9) {
+        if (index >= 7) {
             row.style.display = "none"; // 8번째 이후 데이터 숨김
         }
     });
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
     const rows = document.querySelectorAll(".community tbody tr");
     rows.forEach((row, index) => {
-        if (index >= 11) {
+        if (index >= 9) {
             row.style.display = "none"; // 8번째 이후 데이터 숨김
         }
     });
